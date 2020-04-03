ba2600bf1985a127cdc12ab79eeba9b0a376e48c
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 15:08:32 -0400

    added function in coffee module

--- app/src/main/java/info/sanaebadi/dagger_tutorial/module/CoffeeModule.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/module/CoffeeModule.kt
@@ -1,5 +1,11 @@
 package info.sanaebadi.dagger_tutorial.module
 
+import info.sanaebadi.dagger_tutorial.model.River
+
 class CoffeeModule {
 
+
+    fun provideRiver(): River {
+        return River()
+    }
 }
