84f1c2c8cf4a9efeb80f712ed9694a6dcf601d9e
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 15:11:44 -0400

    added module to component interface

--- app/src/main/java/info/sanaebadi/dagger_tutorial/component/CoffeeComponent.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/component/CoffeeComponent.kt
@@ -3,9 +3,10 @@ package info.sanaebadi.dagger_tutorial.component
 import dagger.Component
 import info.sanaebadi.dagger_tutorial.MainActivity
 import info.sanaebadi.dagger_tutorial.model.Coffee
+import info.sanaebadi.dagger_tutorial.module.CoffeeModule
 
 
-@Component
+@Component(modules = [CoffeeModule::class])
 interface CoffeeComponent {
     fun getCoffee(): Coffee
 
--- app/src/main/java/info/sanaebadi/dagger_tutorial/module/CoffeeModule.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/module/CoffeeModule.kt
@@ -1,10 +1,16 @@
 package info.sanaebadi.dagger_tutorial.module
 
+import dagger.Module
+import dagger.Provides
 import info.sanaebadi.dagger_tutorial.model.River
 
+
+
+@Module
 class CoffeeModule {
 
 
+    @Provides
     fun provideRiver(): River {
         return River()
     }
