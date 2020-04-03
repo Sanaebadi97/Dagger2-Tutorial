6f5898449c6a36a342f70af46aea890bf93870a7
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 13:03:12 -0400

    using component in main activity

--- app/src/main/java/info/sanaebadi/dagger_tutorial/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/MainActivity.kt
@@ -2,6 +2,8 @@ package info.sanaebadi.dagger_tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import info.sanaebadi.dagger_tutorial.component.CoffeeComponent
+import info.sanaebadi.dagger_tutorial.component.DaggerCoffeeComponent
 import info.sanaebadi.dagger_tutorial.model.Coffee
 import info.sanaebadi.dagger_tutorial.model.Farm
 import info.sanaebadi.dagger_tutorial.model.River
@@ -9,12 +11,14 @@ import info.sanaebadi.dagger_tutorial.model.River
 class MainActivity : AppCompatActivity() {
 
 
+    lateinit var coffeeComponent: CoffeeComponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        coffeeComponent = DaggerCoffeeComponent.create()
+        coffeeComponent.getCoffeeCup()
 
     }
 }
