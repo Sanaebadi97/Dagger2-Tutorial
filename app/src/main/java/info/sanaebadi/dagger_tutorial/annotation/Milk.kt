2cb097e002801e6d15ffef7f3bdb218be4101009
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 16:25:50 -0400

    changed the deprecated to newer

--- app/src/main/java/info/sanaebadi/dagger_tutorial/annotation/Milk.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/annotation/Milk.kt
@@ -1,13 +1,10 @@
 package info.sanaebadi.dagger_tutorial.annotation
 
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 import javax.inject.Qualifier
 
 @Qualifier
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
+@MustBeDocumented
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class Milk(
     /** The name.  */
     val value: String = "milk"
