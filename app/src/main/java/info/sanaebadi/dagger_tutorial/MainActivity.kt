d2b358d8bc9c68b52f2eec5db9ac35a23030fd4b
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 14:06:58 -0400

    change code and remove component

--- app/src/main/java/info/sanaebadi/dagger_tutorial/MainActivity.kt
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/MainActivity.kt
@@ -5,11 +5,16 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import info.sanaebadi.dagger_tutorial.component.CoffeeComponent
 import info.sanaebadi.dagger_tutorial.component.DaggerCoffeeComponent
+import info.sanaebadi.dagger_tutorial.model.Coffee
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var coffeeComponent: CoffeeComponent
+    //  lateinit var coffeeComponent: CoffeeComponent
+
+    @Inject
+    lateinit var coffee: Coffee
 
     companion object {
         const val TAG: String = "MainActivity"
@@ -20,10 +25,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        coffeeComponent = DaggerCoffeeComponent.create()
-        coffeeComponent.getCoffee().getCoffeeCup()
+        //   coffeeComponent = DaggerCoffeeComponent.create()
+        //coffeeComponent.getCoffee().getCoffeeCup()
 
-        Log.i(TAG, coffeeComponent.getCoffee().getCoffeeCup())
+        coffee.getCoffeeCup()
+
+        Log.i(TAG, coffee.getCoffeeCup())
 
     }
 }
