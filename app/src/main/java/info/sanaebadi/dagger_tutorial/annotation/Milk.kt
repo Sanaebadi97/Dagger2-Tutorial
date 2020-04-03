0d25c6cceb9cca0ffbd349a2dfcfa0687df9b62b
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-03 16:25:00 -0400

    created Milk annotation

---
+++ app/src/main/java/info/sanaebadi/dagger_tutorial/annotation/Milk.kt
@@ -0,0 +1,14 @@
+package info.sanaebadi.dagger_tutorial.annotation
+
+import java.lang.annotation.Documented
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Qualifier
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+annotation class Milk(
+    /** The name.  */
+    val value: String = "milk"
+)
